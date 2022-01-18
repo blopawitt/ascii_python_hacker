411206413095c655881f2e9b4ac15b043c58254b
Pablo Witt <88154429+blopawitt@users.noreply.github.com>
2022-01-18 20:55:22 -0300

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,11 @@
+#Pablo Santos
+#Sempre gostei mais do sobrenome Witt do que Santos por ser menos comum (I always liked my more my middle name other than my last name)
+print("""
+PPPPPPPPp         SSSSSSS
+P        p      SS
+P        p      SS
+PpppppppP         SSSSSSS
+P                       SS  
+P               SS      SS 
+P                 SSSSSSS    
+""")
